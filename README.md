211fde77b53182014c9c50fa416764a4275575a9
orian <orianhit@hit.ac.il>
2022-04-14 13:51:08 +0300

    style & README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+## howto
+
+run using
+```bash
+gcc -pthread -std=gnu99 main.c && ./a.out
+```
--- main.c
+++ main.c
@@ -5,38 +5,45 @@
 
 sem_t mutex[5];
 
-void increase_other_sems(int current_mux) {
-    for(int i=0; i<5;i++) {
-        if(i != current_mux)
+void increase_other_sems(int current_mux)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (i != current_mux)
             sem_post(&mutex[i]);
     }
 }
 
-void decrease_mux_to_zero(int current_mux) {
-    for(int i=1; i <= 4; i++)
+void decrease_mux_to_zero(int current_mux)
+{
+    for (int i = 1; i <= 4; i++)
         sem_wait(&mutex[current_mux]);
 }
 
-void wait_until_value_four(int current_mux) {
+void wait_until_value_four(int current_mux)
+{
     int sem_value;
 
-    do {
+    do
+    {
         sem_getvalue(&mutex[current_mux], &sem_value);
-    } while(sem_value < 4);
+    } while (sem_value < 4);
 }
 
-void sem_action(int to_print, int current_mux) {
+void sem_action(int to_print, int current_mux)
+{
     printf("\t%d\t\tfrom sem %d\n", to_print, current_mux + 1);
 }
-  
-void* thread(void* arg)
+
+void *print_with_five(void *arg)
 {
-    int current_mux = *((int*)arg);
+    int current_mux = *((int *)arg);
     int next_to_print;
     int counter = 0;
 
-    while(1) {
-        next_to_print = counter*5 + (current_mux+1);
+    while (1)
+    {
+        next_to_print = counter * 5 + (current_mux + 1);
 
         if (next_to_print > 100)
             break;
@@ -45,31 +52,31 @@ void* thread(void* arg)
 
         decrease_mux_to_zero(current_mux);
 
-        sem_action(next_to_print, current_mux );
-            
+        sem_action(next_to_print, current_mux);
+
         increase_other_sems(current_mux);
 
         counter++;
     }
 }
-  
-  
+
 int main()
 {
-    for(int i=0;i<5;i++)
+    for (int i = 0; i < 5; i++)
         sem_init(&mutex[i], 0, 4 - i);
 
     pthread_t t[5];
-    int threadNum[5] = {0,1,2,3,4};
+    int threadNum[5] = {0, 1, 2, 3, 4};
 
-    for(int i=0;i<5;i++) {
-        pthread_create(&t[i],NULL,thread,&threadNum[i]);
+    for (int i = 0; i < 5; i++)
+    {
+        pthread_create(&t[i], NULL, print_with_five, &threadNum[i]);
     }
 
-    for(int i=0;i<5;i++)
-        pthread_join(t[i],NULL);
+    for (int i = 0; i < 5; i++)
+        pthread_join(t[i], NULL);
 
-    for(int i=0;i<5;i++)
+    for (int i = 0; i < 5; i++)
         sem_destroy(&mutex[i]);
 
     return 0;
